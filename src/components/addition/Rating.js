700f7a079355b80d3b063755acc6a64f4ce74284
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2023-01-30 16:16:17 -0800

    Put style to Review component

--- src/components/addition/Rating.js
+++ src/components/addition/Rating.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Grid, Paper, Avatar, Typography, TextField, Button, Checkbox, FormGroup, FormHelperText} from '@material-ui/core';
-
+import { Grid, Paper, Typography, TextField } from '@material-ui/core';
+import CardHeader from '@mui/material/CardHeader';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -10,17 +10,25 @@ const useStyles = makeStyles((theme) => ({
   formControl: {
     margin: theme.spacing(3),
   },
+  header: {
+    backgroundColor: "#2C74B3",
+    height: '4rem'
+  },
+  text: {
+    color: "#fff"
+  }
 }));
 
 const Rating = ({ movie }) => {
   const classes = useStyles();
-  const paperStyle={padding: '50px 20px 80px', width: 700, margin: "80px auto", height: 850, backgroundColor: "#FEFBE7"}
+  const paperStyle={padding: '0px 0px 80px', width: 700, margin: "80px auto", height: 850, backgroundColor: "#FEFBE7"}
   
   return (
     <div>
       <Grid align='center'>
         <Paper elevation={20} style={paperStyle}>
-
+          <CardHeader className={classes.header} title={<h3 className={classes.text}>Review for {movie.name}</h3>}>
+          </CardHeader>
         </Paper>
       </Grid>
     </div>
