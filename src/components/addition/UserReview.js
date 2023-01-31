40c88dc1ab77b02281814ee5eaf30abb236cf9d1
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2023-01-31 14:54:50 -0800

    Modify FormControl fields

--- src/components/addition/UserReview.js
+++ src/components/addition/UserReview.js
@@ -1,9 +1,21 @@
 import React, { Fragment, useState } from 'react'
 import { Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import { Container, FormControl, Grid, IconButton, List, ListItem, TextField } from '@material-ui/core';
+import { Container, FormControl, Grid, List, ListItem, TextField } from '@material-ui/core';
 import { MovieReviewDto } from '../MovieReviewDto';
-import SendIcon from '@mui/icons-material/Send';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex'
+  },
+  formControl: {
+    margin: theme.spacing(3),
+  },
+  textField: {
+    margin: 10
+  }
+}));
 
 const UserReview = () => {
 
@@ -11,6 +23,7 @@ const UserReview = () => {
     new MovieReviewDto('John', 'Hi, everyone! This is awesome movie!')
   ])
 
+  const classes = useStyles();
   const [user, setUser] = useState('')
   const [review, setReview] = useState('')
 
@@ -29,43 +42,36 @@ const UserReview = () => {
 
   return (
     <Fragment>
-      {/* <Container>
-        <Paper elevation={2}>
-          <Box p={3}> */}
-            {/* <Typography variant='h6' gutterBottom>
-              Hello World!
-            </Typography>
-            <Divider/> */}
-            <Grid container spacing={3} alignItems='center'>
-              <Grid xs={12} style={{height: '32rem'}} item>
-                <List style={{height: '30rem', overflow: 'auto'}}>
-                  {listMovieReviews}
-                </List>
-              </Grid>
-              <Container style={{height: '14rem'}}>
-                <Grid container sx={{mb: 5}}>
-                  <FormControl fullWidth>
-                    <TextField
-                      value={user}
-                      label='User'
-                      variant='standard'
-                    />
-                  </FormControl>
-                </Grid>
-                <Grid item>
-                  <FormControl fullWidth>
-                    <TextField
-                      value={review}
-                      label='Your review..'
-                      variant='outlined'
-                    />                    
-                  </FormControl>                
-                </Grid>
-              </Container>
-            </Grid>
-          {/* </Box>
-        </Paper> */}
-      {/* </Container> */}
+      <Grid container direction={'column'} spacing={6} alignItems='center'>
+        <Grid xs={12} style={{height: '32rem'}} item>
+          <List style={{height: '30rem', overflow: 'auto'}}>
+            {listMovieReviews}
+          </List>
+        </Grid>
+        <Container style={{height: '14rem'}}>
+          <Grid container >
+            <FormControl fullWidth className={classes.formControl}>
+              <TextField className={classes.textField}
+                value={user}
+                label='User'
+                placeholder='Insert user name...'
+                variant='standard'
+              />
+              <TextField className={classes.textField}
+                value={review}
+                label='Review'
+                placeholder='Type your review...'
+                variant='outlined'
+              />        
+            </FormControl>
+          </Grid>
+          {/* <Grid item xs={12} md={6} sx={{m: 5}}>
+            <FormControl fullWidth>
+                          
+            </FormControl>                
+          </Grid> */}
+        </Container>
+      </Grid>
     </Fragment>
   )
 }
